e3a15460a2dc264eef8abe56b12a59b21203c105
Ilya Sterin <isterin@gmail.com>
2012-01-24 11:59:34 -0500

    Clojure mode

--- customizations/modes.el
+++ customizations/modes.el
@@ -13,3 +13,35 @@
 ;; This is ugly, find a way to use env variables
 (getmyenv "ENSIME_HOME") ;; Needed to make the ensime script succeed
 (set-variable 'ensime-default-server-cmd "~/.emacs.d/vendor/ensime/server")
+
+
+;; Lisp
+(setq inferior-lisp-program "/usr/local/bin/sbcl"
+  ;; Change me!
+  ; slime-path (expand-file-name "~/.emacs.d/vendor/slime/")
+  ; w3m-command "/usr/local/bin/w3m"
+  ; cltl2-url "file:///usr/share/doc/cltl/clm/node1.html"
+  ; hyperspec-prog (concat slime-path "hyperspec")
+  ; hyperspec-path "/usr/share/doc/hyperspec/"
+  ; lisp-indent-function 'common-lisp-indent-function
+  ; slime-complete-symbol-function 'slime-fuzzy-complete-symbol
+  ; common-lisp-hyperspec-root (concat "file://" hyperspec-path)
+  ; common-lisp-hyperspec-symbol-table (concat hyperspec-path "Data/Map_Sym.txt")
+  ; w3m-default-homepage "file:///usr/share/doc/hyperspec/Front/X_Symbol.htm"
+  ; browse-url-browser-function '(("." . w3m-browse-url-other-window))
+  ; w3m-symbol 'w3m-default-symbol
+  ; w3m-key-binding 'info
+  ; w3m-coding-system 'utf-8
+  ; w3m-default-coding-system 'utf-8
+  ; w3m-file-coding-system 'utf-8
+  ; w3m-file-name-coding-system 'utf-8
+  ; w3m-terminal-coding-system 'utf-8
+  )
+
+
+(eval-after-load 'slime
+  '(progn
+    ; (define-key slime-mode-map (kbd "TAB") 'slime-indent-and-complete-symbol)
+    (add-to-list 'slime-lisp-implementations 
+      '(sbcl ("/usr/local/bin/sbcl")))))
+            
