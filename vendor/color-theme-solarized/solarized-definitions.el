0f1052cf933297cf3f4ba825bda64286ab00d5d0
Ilya Sterin <isterin@gmail.com>
2012-01-28 11:50:03 -0500

    Fixed cursor

--- vendor/color-theme-solarized/solarized-definitions.el
+++ vendor/color-theme-solarized/solarized-definitions.el
@@ -114,8 +114,10 @@ in order to expand or compress the tonal range displayed."
                      opt-under t)))
         `((;; basic
            (default ((t (:foreground ,base0 ,:background ,back))))
-           (cursor
-            ((t (:foreground ,base0 :background ,base03 :inverse-video t))))
+		  		 (cursor
+						 ((t (:foreground ,base02 :background ,base1 :inverse-video t))))
+           ; (cursor
+           ;  ((t (:foreground ,base0 :background ,base03 :inverse-video t))))
            (escape-glyph-face ((t (:foreground ,red))))
            (fringe ((t (:foreground ,base01 :background ,base02))))
            (linum ((t (:foreground ,base01 :background ,base02))))
